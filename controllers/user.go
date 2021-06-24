8236ce802cc23a5f8af205775a582dde7cc4a434
Jeffrey Wallace <jeffrey.wallace81@gmail.com>
2021-06-24 15:54:38 -0500

    Completed go-webservice

--- controllers/user.go
+++ controllers/user.go
@@ -1,8 +1,12 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
 	"regexp"
+	"strconv"
+
+	"github.com/jw81/go-webservice/models"
 )
 
 type userController struct {
@@ -10,7 +14,105 @@ type userController struct {
 }
 
 func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from the User Controller!"))
+	if r.URL.Path == "/users" {
+		switch r.Method {
+		case http.MethodGet:
+			uc.getAll(w, r)
+		case http.MethodPost:
+			uc.post(w, r)
+		default:
+			w.WriteHeader(http.StatusNotImplemented)
+		}
+	} else {
+		matches := uc.userIDPattern.FindStringSubmatch(r.URL.Path)
+		if len(matches) == 0 {
+			w.WriteHeader(http.StatusNotFound)
+		}
+		id, err := strconv.Atoi(matches[1])
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+		}
+		switch r.Method {
+		case http.MethodGet:
+			uc.get(id, w)
+		case http.MethodPut:
+			uc.put(id, w, r)
+		case http.MethodDelete:
+			uc.delete(id, w)
+		default:
+			w.WriteHeader(http.StatusNotImplemented)
+		}
+	}
+}
+
+func (uc *userController) getAll(w http.ResponseWriter, r *http.Request) {
+	encodeResponseAsJSON(models.GetUsers(), w)
+}
+
+func (uc *userController) get(id int, w http.ResponseWriter) {
+	user, err := models.GetUserById(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	encodeResponseAsJSON(user, w)
+}
+
+func (uc *userController) post(w http.ResponseWriter, r *http.Request) {
+	user, err := uc.parseRequest(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("could not parse user object"))
+		return
+	}
+	user, err = models.AddUser(user)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	encodeResponseAsJSON(user, w)
+}
+
+func (uc *userController) put(id int, w http.ResponseWriter, r *http.Request) {
+	user, err := uc.parseRequest(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("could not parse user object"))
+		return
+	}
+	if id != user.ID {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("id of submitted user must match id in url"))
+		return
+	}
+	user, err = models.UpdateUser(user)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	encodeResponseAsJSON(user, w)
+}
+
+func (uc *userController) delete(id int, w http.ResponseWriter) {
+	err := models.RemoveUserById(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func (uc *userController) parseRequest(r *http.Request) (models.User, error) {
+	dec := json.NewDecoder(r.Body)
+	var user models.User
+	err := dec.Decode(&user)
+	if err != nil {
+		return models.User{}, err
+	}
+	return user, nil
 }
 
 func newUserController() *userController {
--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 type User struct {
 	ID        int
 	FirstName string
@@ -16,8 +21,44 @@ func GetUsers() []*User {
 }
 
 func AddUser(user User) (User, error) {
+	if user.ID != 0 {
+		return User{}, errors.New("new user must not include id or it must be set to zero")
+	}
+
 	user.ID = nextID
 	nextID++
 	users = append(users, &user)
 	return user, nil
+}
+
+func GetUserById(id int) (User, error) {
+	for _, user := range users {
+		if user.ID == id {
+			return *user, nil
+		}
+	}
+
+	return User{}, fmt.Errorf("user with id '%value' not found", id)
+}
+
+func UpdateUser(user User) (User, error) {
+	for i, candidate := range users {
+		if candidate.ID == user.ID {
+			users[i] = &user
+			return user, nil
+		}
+	}
+
+	return User{}, fmt.Errorf("user with id '%value' not found", user.ID)
+}
+
+func RemoveUserById(id int) error {
+	for i, user := range users {
+		if user.ID == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("user with id '%value' not found", id)
 }
